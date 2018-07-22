6eb7db55568857e8d76fedd6fb9a58675218ca9c
Stu Halliburton <stu.halliburton@gmail.com>
2018-07-22 16:46:48 +0100

    further tweaks/refactors

--- train_nn.py
+++ train_nn.py
@@ -1,10 +1,11 @@
 import keras
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.layers.normalization import BatchNormalization
 import numpy
 
 num_classes = 5
-epoch = 100
+epoch = 1000
 batch_size = 10
 
 dataset = numpy.loadtxt("data/parsed.csv", delimiter=",")
@@ -14,9 +15,9 @@ y_train = dataset[:,25]
 y_train = keras.utils.to_categorical(y_train, num_classes)
 
 model = Sequential()
-model.add(keras.layers.normalization.BatchNormalization())
+model.add(BatchNormalization())
 model.add(Dense(625, input_dim=25, kernel_initializer="uniform", activation="relu"))
-model.add(Dense(625, kernel_initializer="uniform", activation="relu"))
+model.add(Dense(50, kernel_initializer="uniform", activation="relu"))
 model.add(Dense(5, kernel_initializer="uniform", activation="softmax"))
 
 #opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)
