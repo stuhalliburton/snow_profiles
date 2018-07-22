853a06922efb831a708ad681e6680080c2f11b78
Stu Halliburton <stu.halliburton@gmail.com>
2018-07-22 15:14:40 +0100

    add validation data split

--- train_nn.py
+++ train_nn.py
@@ -15,15 +15,15 @@ y_train = keras.utils.to_categorical(y_train, num_classes)
 
 model = Sequential()
 model.add(keras.layers.normalization.BatchNormalization())
-model.add(Dense(512, input_dim=25, kernel_initializer="uniform", activation="relu"))
-model.add(Dense(512, kernel_initializer="uniform", activation="relu"))
+model.add(Dense(625, input_dim=25, kernel_initializer="uniform", activation="relu"))
+model.add(Dense(625, kernel_initializer="uniform", activation="relu"))
 model.add(Dense(5, kernel_initializer="uniform", activation="softmax"))
 
 #opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)
 #model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
 model.compile(loss="categorical_crossentropy", optimizer="rmsprop", metrics=["accuracy"])
 
-model.fit(x_train, y_train, epochs=epoch, batch_size=batch_size)
+model.fit(x_train, y_train, epochs=epoch, batch_size=batch_size, validation_split=0.1)
 
 scores = model.evaluate(x_train, y_train)
 print("Test loss:", scores[0])
