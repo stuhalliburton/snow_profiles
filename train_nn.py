3470fef0815c33376374afe9274e7bdc6ec50b25
Stu Halliburton <stu.halliburton@gmail.com>
2018-07-25 15:24:14 +0100

    remove normalisation, increase batch size and split dataset into train/test

--- train_nn.py
+++ train_nn.py
@@ -1,21 +1,23 @@
 import keras
 from keras.models import Sequential
 from keras.layers import Dense
-from keras.layers.normalization import BatchNormalization
 import numpy
 
 num_classes = 5
 epoch = 1000
-batch_size = 10
+batch_size = 500
+test_size = 200
 
 dataset = numpy.loadtxt("data/parsed.csv", delimiter=",")
-x_train = dataset[:,0:25]
-y_train = dataset[:,25]
+numpy.random.shuffle(dataset)
+features = dataset[:, 0:25]
+labels = dataset[:, 25]
+labels = keras.utils.to_categorical(labels, num_classes)
 
-y_train = keras.utils.to_categorical(y_train, num_classes)
+x_train, x_test = features[test_size:], features[:test_size]
+y_train, y_test = labels[test_size:], labels[:test_size]
 
 model = Sequential()
-model.add(BatchNormalization())
 model.add(Dense(625, input_dim=25, kernel_initializer="uniform", activation="relu"))
 model.add(Dense(50, kernel_initializer="uniform", activation="relu"))
 model.add(Dense(5, kernel_initializer="uniform", activation="softmax"))
@@ -26,6 +28,6 @@ model.compile(loss="categorical_crossentropy", optimizer="rmsprop", metrics=["ac
 
 model.fit(x_train, y_train, epochs=epoch, batch_size=batch_size, validation_split=0.1)
 
-scores = model.evaluate(x_train, y_train)
+scores = model.evaluate(x_test, y_test)
 print("Test loss:", scores[0])
 print("Test accuracy:", scores[1])
